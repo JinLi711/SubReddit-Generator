b6b93619e6b7553b22de8fbb66f863795b7b3e7e
Alex Oro <alexoro@uchicago.edu>
2019-02-16 12:45:32 -0600

    Pulled some comments

---
+++ reddit.py
@@ -0,0 +1,34 @@
+import praw
+import json
+from praw.models import MoreComments
+
+reddit = praw.Reddit(client_id='DFv008_q4N3hiQ', client_secret='vA0dVBIxydeISNdEJ9yA0UIL58g', user_agent='cool by /u/BottyMcBotFace123')
+
+shower = reddit.subreddit('ShowerThoughts')
+
+textFile = open("thoughts.txt", "w")
+
+textFile.write('[')
+
+for post in shower.top(limit=10):
+  newPost = { 
+    'title': post.title,
+    'text': post.selftext
+  }
+  json.dump(newPost, textFile)
+  textFile.write(',\n')
+  i = 0
+  for comment in post.comments:
+    i += 1
+    if i > 10:
+      continue
+    if isinstance(comment, MoreComments):
+      continue
+    newComment = {
+      'text': comment.body
+    }
+    json.dump(newComment, textFile)
+    textFile.write(',\n')
+
+textFile.write(']')
+
